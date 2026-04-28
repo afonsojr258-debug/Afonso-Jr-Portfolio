0adcf498aef0b66e813be01c6737d41ee13de1ad
Afonso Jr <afonsojr258@gmail.com>
2026-04-28 07:00:13 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("mn")
+
+const opt = document.getElementById("menu-o")
+
+menu.addEventListener("click", () => {
+  opt.classList.toggle("show")
+})
+
+function what(){
+  location.href = "https://wa.me/258842167459?text=Venho%20do%20seu%20*portf%C3%B3lio*,%20podemos%20conversar?⁠"
+}
